b3c15a128dfcbd4de730e62ad5db89dfb9a534fc
Enoc <etorra@gmail.com>
2017-07-30 12:14:10 -0400

    fixed profile page

--- index.js
+++ index.js
@@ -10,18 +10,30 @@ app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.render('home', {users:data.users})
+app.get('/', function(req, res) {
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].job === null) {
+      data.users[i].job = "Available for hire";
+    }
+  }
+  res.render('home', {
+    users: data.users
+  })
 });
 
-app.get('/:data', function (req, res) {
-  res.render('profile', {users:data.users});
-
+app.get('/:profile', function(req, res) {
+  let robot = req.params.profile;
+  for (let i = 0; i < data.users.length; i++) {
+    if (data.users[i].username === robot) {
+      res.render('profile', {
+        users: data.users[i]
+      });
+    }
+  }
 });
-
 
 console.log(data);
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log('Successfully started express app');
 });
